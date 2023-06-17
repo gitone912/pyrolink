f9386d8169782049ef0a2ede4098968d4602958d
gitone912 <akash.101@kvsroranchi.in>
2023-06-17 19:16:28 +0530

    se

--- userAuth/models.py
+++ userAuth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Add custom fields if needed
+    pass
