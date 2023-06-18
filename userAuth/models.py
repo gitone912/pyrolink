81dd0155024ad6330c55fe38711d8f54b0eb7913
gitone912 <akash.101@kvsroranchi.in>
2023-06-18 14:10:12 +0530

    edits

--- userAuth/models.py
+++ userAuth/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     # Add custom fields if needed
-    pass
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='user'
+    )
