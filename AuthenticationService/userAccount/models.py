492193fb5dd4bb1ca9f9abff66a8fb9e1102145a
gitone912 <akash.101@kvsroranchi.in>
2023-06-22 00:27:02 +0530

    u

--- AuthenticationService/userAccount/models.py
+++ AuthenticationService/userAccount/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from userAuth.models import MyUser
 
-# Create your models here.
+#create a model unique for every user and store the data like adress skills they like etc
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+#     address = models.CharField(max_length=200)
+#     skills = models.CharField(max_length=200)
+#     interests = models.CharField(max_length=200)
+#     about = models.CharField(max_length=200)
+#     def __str__(self):
+#         return self.user.name
