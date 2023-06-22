2b1f3c54d87fb4a8d77dfcf166c00701fcfa28b4
gitone912 <akash.101@kvsroranchi.in>
2023-06-22 10:03:33 +0530

    j

--- frontend-react/src/services/userAuthApi.js
+++ frontend-react/src/services/userAuthApi.js
@@ -21,4 +21,4 @@ export const userAuthApi = createApi({
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const {  } = userAuthApi
+export const { useRegisterUserMutation  } = userAuthApi
